987fd22084a7f02ab6cb0215ab1fdfaf2d8d29db
Joe Ng <joengsh@gmail.com>
2022-12-09 06:45:19 +0000

    update eslint config

--- packages/eslint-config/eslint-config.js
+++ packages/eslint-config/eslint-config.js
@@ -6,21 +6,10 @@ module.exports = {
   },
   extends: [
     "eslint:recommended",
-    "plugin:react/recommended",
-    "plugin:@typescript-eslint/recommended",
+    "plugin:@typescript-eslint/recommended",    
+    "react-app",
+    "prettier",
     "plugin:jest/recommended",
     "plugin:prettier/recommended"
-  ],
-  parser: "@typescript-eslint/parser",
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
-    },
-    ecmaVersion: 12,
-    sourceType: "module",
-  },
-  plugins: ["react", "@typescript-eslint", "jest", "import", "prettier"],
-  rules: {
-    "prettier/prettier": "error"
-  }
+  ]
 };
